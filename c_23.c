3de9076d55d8e687be146c5b47c9c443f31ded48
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-05-27 13:00:10 +0530

    update

---
+++ c_23.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int area, side;
+    printf("enter side of square\n");
+    scanf("%d",&side); 
+    area=side*side;
+    printf("area of square=%d\n",area);
+    return 0;
+}
