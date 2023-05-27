d4ec4fcdbad0e75db0fd66286a4b2bf69bb21093
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-05-27 11:58:06 +0530

    update

---
+++ c_21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    double area,r;
+    printf("enter radius of circle\n");
+    scanf("%lf",&r); 
+    area=3.14*r*r;
+    printf("area of circle=%f\n",area); 
+    return 0;
+}
