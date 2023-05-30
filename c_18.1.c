16c8cf5814becb56e7bfab0389ec809a080d5dd2
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-05-30 12:19:50 +0530

    update

--- c_18.1.c
+++ c_18.1.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-int main()
+void main()
 {
 	int s,i;
 	float k=0.0;
-	for(i=1;i<=100;i++)
+	for(i=1;i<100;i++)
 	{
 		k=k+i/((float)i+1);
+		printf("%d,%d\n",i,(i+1));
 	}
 	printf("the sum of the series is %f",k);
-	return 0;
+	//return 0;
 }
