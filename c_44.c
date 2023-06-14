070d642f2dce2e4f739f4b630679db8971d49407
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-06-14 23:36:13 +0530

    update

---
+++ c_44.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("\n enter a number:\n");
+    scanf("%d",&n);
+   printf("\n multiplication table of the number:\n");
+   for(i=1;i<=10;i++);
+   {
+    printf("%d*%d=%d\n",n,i,n*i);
+   }
+   return 0;
+}
