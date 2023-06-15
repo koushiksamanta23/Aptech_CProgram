7fc9c8db14a8fec6537fa6f71fb6062697bd7211
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-06-15 12:03:47 +0530

    update

--- c_44.c
+++ c_44.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    printf("\n enter a number:\n");
-    scanf("%d",&n);
+   int i,n;
+   printf("\n enter a number:\n");
+   scanf("%d",&n);
    printf("\n multiplication table of the number:\n");
-   for(i=1;i<=10;i++);
-{
-   printf("%d*%d=%d\n",n,i,n*i);
-}
+   for(i=1;i<=10;i++)
+   {
+      printf("%d*%d=%d\n",n,i,(n*i));
+   }
    return 0;
 }
