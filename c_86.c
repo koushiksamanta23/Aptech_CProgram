4446779697fd395b32279ca6cfe804d2507ea337
koushiksamanta23 <koushiksamanta23@hotmail.com>
2023-07-27 17:01:30 +0530

    update

---
+++ c_86.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**************************************************/
+/*       PROGRAM SHOWING THE USE OF A CASE       */
+/************************************************/
+int main()
+{
+    float sum;
+    int n;
+    sum = 0;
+    for(n = 1; n <= 10; ++n);
+    {
+        sum = sum + 1/(float)n;
+        printf("%2d %6.4f\n",n,sum);
+    }
+    return 0;
+}
