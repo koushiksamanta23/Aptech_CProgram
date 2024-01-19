aa49b65163d2ea3c4677ac8dc7bafd273d36eff7
koushiksamanta23 <koushiksamanta23@hotmail.com>
2024-01-19 14:26:36 +0530

    update

--- cpp/c_.cpp
+++ cpp/c_.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     int num1,num2,temp;
-    cout<<"enter the value of first veriable:  ";
+    cout<<"enter the value of first variable:  ";
     cin>>num1;
-    cout<<"enter the value of second veriable: ";
+    cout<<"enter the value of second variable: ";
     cin>>num2;
-    cout<<"before swapping the value of veriable 1 = "<<num1<<" and the value of veriable 2 = "<<num2<<endl;
+    cout<<"before swapping the value of variable 1 = "<<num1<<" and the value of variable 2 = "<<num2<<endl;
     temp=num1;
     num1=num2;
     num2=temp;
-    cout<<"after swapping the value of veriable 1 = "<<num1<<" and the value of veriable 2 ="<<num2<<endl;
+    cout<<"after swapping the value of variable 1 = "<<num1<<" and the value of variable 2 = "<<num2<<endl;
     return 0;
 }
--- cpp/c_03.cpp
+++ cpp/c_03.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"enter the value of first veriable:  ";
+    cout<<"enter the value of first variable:  ";
     cin>>a;
-    cout<<"enter the value of second veriable: ";
+    cout<<"enter the value of second variable: ";
     cin>>b;
-    cout<<"before swapping the value of veriable 1 = "<<a<<" and the value of veriable 2 = "<<b<<endl;
+    cout<<"before swapping the value of variable 1 = "<<a<<" and the value of variable 2 = "<<b<<endl;
     a=a*b;
     b=a/b;
     a=a/b;
-    cout<<"after swapping the value of veriable 1 = "<<a<<" and the value of veriable 2 ="<<b<<endl;
+    cout<<"after swapping the value of variable 1 = "<<a<<" and the value of variable 2 ="<<b<<endl;
     return 0;
 }
